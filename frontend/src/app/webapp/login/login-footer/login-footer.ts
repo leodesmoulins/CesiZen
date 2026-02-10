3945a8cce2934536d8c038909e445b823cd43357
Desmoulins, Leo <leo.desmoulins+claascp@claas.com>
2026-02-10 23:18:03 +0100

    login component init

---
+++ frontend/src/app/webapp/login/login-footer/login-footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-footer',
+  imports: [],
+  templateUrl: './login-footer.html',
+  styleUrl: './login-footer.css',
+})
+export class LoginFooter {
+
+}
