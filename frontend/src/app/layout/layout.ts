f02528bd2ffae2bd64a5f8db4f05a43cabab4f99
Desmoulins, Leo <leo.desmoulins+claascp@claas.com>
2026-02-20 11:17:28 +0100

    layout

--- frontend/src/app/layout/layout.ts
+++ frontend/src/app/layout/layout.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Navbar } from '../shared/include/navbar/navbar';
+import { Sidebar } from '../shared/include/sidebar/sidebar';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from '../shared/include/footer/footer';
+import { AuthenticationApplication } from '../shared/services/auth/authentication.application';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [Navbar, Sidebar, RouterOutlet, Footer],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
 export class Layout {
-
+  private authApp = inject(AuthenticationApplication);
+  readonly isAuthenticated = this.authApp.isAuthenticated;
 }
